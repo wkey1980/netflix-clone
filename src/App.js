47391555be5bb86da1b75ce036d60fff215b96d8
Will <28814921+wkey1980@users.noreply.github.com>
2021-05-31 19:43:58 +0100

    Building the Header.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Header'
 import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Login />
     </div>
   );
