4e881f67908a038afb699caf253696a0ccc7d8be
Will <28814921+wkey1980@users.noreply.github.com>
2021-06-05 16:28:28 +0100

    Reset NETFLIX-clone React App for learning code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Header from './components/Header'
-import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Login />
+
     </div>
   );
 }
