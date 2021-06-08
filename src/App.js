8a6045db4dd59c903a58ff1598050aa6153caebe
Will <28814921+wkey1980@users.noreply.github.com>
2021-06-08 12:34:25 +0100

    Created globalStyles.js & added to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import GlobalStyle from "./globalStyles";
+
 function App() {
 	return (
 		<div className="App">
-
+			<GlobalStyle />
+			{/* <Login /> */}
+			{/* <Header /> */}
+			{/* <Navbar /> */}
+			{/* <Logo /> */}
+			{/* <SignIn /> */}
+			{/* <Jumbotron-1 /> */}
+			{/* <Jumbotron-2 /> */}
+			{/* <Jumbotron-3 /> */}
+			{/* <Jumbotron-4 /> */}
+			{/* <Faqs /> */}
+			{/* <Footer /> */}
 		</div>
 	);
 }
